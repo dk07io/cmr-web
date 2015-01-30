f443ea5e8986b34e14e41fe76c0a566120e8f687
Daniel Kim <dk07io@Daniels-MacBook-Pro-3.local>
2015-01-30 18:38:30 -0500

    Updated the get route to use DataMapper

--- crm.rb
+++ crm.rb
@@ -31,22 +31,27 @@ get '/' do
   erb :index
 end
 
-get '/contacts' do
+get "/contacts" do
+  @contacts = Contact.all
   erb :contacts
 end
+
+# get '/contacts' do
+#   erb :contacts
+# end
 
 get '/contacts/new' do 
   erb :new_contact
 end
 
-post '/contacts' do
-  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-  $rolodex.add_contact(new_contact)
-  redirect to('/contacts')
-end
+# post '/contacts' do
+#   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+#   $rolodex.add_contact(new_contact)
+#   redirect to('/contacts')
+# end
 
 get "/contacts/:id" do
-  @contact = $rolodex.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
     erb :show_contact
   else
@@ -54,19 +59,29 @@ get "/contacts/:id" do
   end
 end
 
-put "/contacts/:id" do
-  @contact = $rolodex.find(params[:id].to_i)
-  if @contact
-    @contact.first_name = params[:first_name]
-    @contact.last_name = params[:last_name]
-    @contact.email = params[:email]
-    @contact.note = params[:note]
-
-    redirect to("/contacts")
-  else
-    raise Sinatra::NotFound
-  end
+post "/contacts" do
+  contact = Contact.create(
+    :first_name => params[:first_name],
+    :last_name => params[:last_name],
+    :email => params[:email],
+    :note => params[:note]
+  )
+  redirect to('/contacts')
 end
+
+# put "/contacts/:id" do
+#   @contact = $rolodex.find(params[:id].to_i)
+#   if @contact
+#     @contact.first_name = params[:first_name]
+#     @contact.last_name = params[:last_name]
+#     @contact.email = params[:email]
+#     @contact.note = params[:note]
+
+#     redirect to("/contacts")
+#   else
+#     raise Sinatra::NotFound
+#   end
+# end
 
 get "/contacts/:id/edit" do
   @contact = $rolodex.find(params[:id].to_i)
